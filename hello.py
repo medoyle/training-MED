0a4b4f11f55e3f0e6921302e8ff9c6c262968624
maired <maired@ip-172-31-93-109.ec2.internal>
2020-05-15 18:27:32 +0000

    my first python programme

---
+++ hello.py
@@ -0,0 +1,34 @@
+# This is  my first python program
+
+name="Raj"
+age=22
+
+
+brothername="Vijay"
+brotherage=26
+
+agedifference=age-brotherage
+difference=abs(agedifference)
+
+print("My Name is: " , name)
+print("My Age is :", age)
+print("My brothers Name is:", brothername)
+print("My brothers Age is:",brotherage)
+print("We were born", difference, "years apart")
+
+
+
+
+
+
+
+print("My name is: " , name)
+print("My Age is:  " ,  years)
+
+
+
+
+
+
+print("Hello World")
+
